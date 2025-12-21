4ce235869e7d6777402789e5d26c65cb83c4e25b
marco <marcoramirezmail@gmail.com>
2025-12-21 11:29:24 -0800

    mobile nav bug fix -- design start

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,8 +1,6 @@
 const menu = document.querySelector(".mobile-button");
 
 menu?.addEventListener("click", () => {
-  console.log("success");
-
   const isExpanded = menu.getAttribute("aria-expanded") === "true";
   menu.setAttribute("aria-expanded", `${!isExpanded}`);
 });
