088b9465c223d10d449629b265c092d5af75672b
marco <marcoramirezmail@gmail.com>
2025-12-21 10:53:59 -0800

    mobile nav start ++ added astro-icons to make life easier

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact(), icon()]
 });
--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,8 @@
-const menu = document.querySelector(".menu");
+const menu = document.querySelector(".mobile-button");
 
 menu?.addEventListener("click", () => {
+  console.log("success");
+
   const isExpanded = menu.getAttribute("aria-expanded") === "true";
   menu.setAttribute("aria-expanded", `${!isExpanded}`);
 });
