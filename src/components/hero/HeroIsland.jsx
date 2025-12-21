8b671964be2adda4a5c694fa5d38a7a70cc08e7d
marco <marcoramirezmail@gmail.com>
2025-12-21 13:38:47 -0800

    created hero island for gsap animations

---
+++ src/components/hero/HeroIsland.jsx
@@ -0,0 +1,68 @@
+import * as styles from "../../styles/components/HeroIsland.module.scss";
+import { useLayoutEffect, useRef } from "preact/hooks";
+import gsap from "gsap";
+
+export default function HeroIsland({
+  headerTop,
+  headerBottom,
+  subHeader,
+  buttonActions,
+}) {
+  const root = useRef(null);
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      //   gsap.to(`.${styles.heroHeader}`, {
+      //     y: 0,
+      //     x: 0,
+      //     opacity: 1,
+      //     // duration: 0.8,
+      //     duration: 0.6,
+      //     ease: "power3.out",
+      //   });
+      gsap.to(
+        gsap.utils.selector(root.current)("h1"),
+        {
+          y: 0,
+          x: 200,
+          opacity: 1,
+          duration: 0.8,
+          ease: "power3.out",
+          stagger: 0.12,
+        },
+        root
+      );
+
+      gsap.to(`.${styles.heroSubHeader}`, {
+        y: 0,
+        x: 0,
+        opacity: 1,
+        duration: 0.8,
+        ease: "power3.out",
+      });
+    });
+
+    return () => ctx.revert();
+  }, []);
+
+  return (
+    <section className={styles.heroIsland}>
+      <div ref={root} className={styles.heroHeader}>
+        <h1>{headerTop}</h1>
+        <h1>{headerBottom}</h1>
+      </div>
+
+      <div className={styles.heroSubHeader}>
+        <h2>{subHeader}</h2>
+      </div>
+
+      <div className={styles.heroButtons}>
+        {buttonActions.map(({ href, label }) => (
+          <a className={styles.button} href={href}>
+            <button> {label}</button>
+          </a>
+        ))}
+      </div>
+    </section>
+  );
+}
